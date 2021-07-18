2cb2857f3a8492c978d593585a36edcb00e1c451
Zacharyjg <guldb016@umn.edu>
2021-07-17 19:58:25 -0500

    Added functionality for Add Role and View Employees By Role. Added helper methods for departments

--- employee.js
+++ employee.js
@@ -17,7 +17,7 @@ function start() {
             type: "list",
             message: "What would you like to do?",
             name: "choice",
-            choices: ["View All Employees", "View Employees By Department", "Add Employee", "Update Employee Role", "Add Role", "Add Department", "Exit"]
+            choices: ["View All Employees", "View Employees By Department", "View Employees By Role", "Add Employee", "Update Employee Role", "Add Role", "Add Department", "Exit"]
         }
     ).then(({ choice }) => {
         switch (choice) {
@@ -29,6 +29,10 @@ function start() {
                 viewEmployeesByDepartment();
                 break;
 
+            case "View Employees By Role":
+                viewEmployeesByRole();
+                break;
+
             case "Add Employee":
                 addEmployee();
                 break;
@@ -37,6 +41,7 @@ function start() {
                 break;
 
             case "Add Role":
+                addRole();
                 break;
 
             case "Add Department":
@@ -86,15 +91,15 @@ function getDepartments() {
     let currentDepartments = [];
     return new Promise((resolve, reject) => {
         connection.query(
-            `select d.name from department d;`, (err, results) => {
-            if (err) {
-                reject(new Error(err.message));
-            }
-            results.forEach(element => {
-                currentDepartments.push({ name: element.name });
+            `select d.name, d.id from department d;`, (err, results) => {
+                if (err) {
+                    reject(new Error(err.message));
+                }
+                results.forEach(element => {
+                    currentDepartments.push({ id: element.id, name: element.name });
+                });
+                resolve(currentDepartments);
             });
-            resolve(currentDepartments);
-        });
     });
 }
 
@@ -107,11 +112,28 @@ function getManagerNames(arr) {
 }
 
 function getRoleTitles(arr) {
-    let rolesArr = [];
+    let roleArr = [];
     arr.forEach(role => {
-        rolesArr.push(`${role.title}`);
+        roleArr.push(`${role.title}`);
     });
-    return rolesArr;
+    return roleArr;
+}
+
+function getDepartmentNames(arr) {
+    let departmentArr = [];
+    arr.forEach(department => {
+        departmentArr.push(`${department.name}`);
+    });
+    return departmentArr;
+}
+
+function getDepartmentId(departmentArr, departmentName) {
+    for (departmentData of departmentArr) {
+        let department = `${departmentData.name}`;
+        if (department == departmentName) {
+            return departmentData.id;
+        }
+    }
 }
 
 function getManagerId(managerArr, managerName) {
@@ -151,7 +173,7 @@ async function viewEmployeesByDepartment() {
             type: "list",
             message: "What department would you like to view?",
             name: "departmentName",
-            choices: currentDepartments
+            choices: getDepartmentNames(currentDepartments)
         },
     ]).then(({ departmentName }) => {
         connection.query(
@@ -160,6 +182,29 @@ async function viewEmployeesByDepartment() {
             inner join department d on r.department_id = d.id
             left join employee ee on e.manager_id = ee.id
             where d.name = '${departmentName}';`, (err, result) => {
+            if (err) throw err;
+            console.table(result);
+            start();
+        });
+    });
+}
+
+async function viewEmployeesByRole() {
+    let currentRoles = await getRoles();
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What role would you like to view?",
+            name: "roleTitle",
+            choices: getRoleTitles(currentRoles)
+        },
+    ]).then(({ roleTitle }) => {
+        connection.query(
+            `select e.id, e.first_name, e.last_name, r.title, d.name, r.salary, concat(ee.first_name, " ", ee.last_name) as "manager" from employee e
+            inner join role r on e.role_id = r.id 
+            inner join department d on r.department_id = d.id
+            left join employee ee on e.manager_id = ee.id
+            where r.title = '${roleTitle}';`, (err, result) => {
             if (err) throw err;
             console.table(result);
             start();
@@ -196,7 +241,7 @@ async function addEmployee() {
         },
     ]).then(({ first_name, last_name, role, manager }) => {
         connection.query(
-            'INSERT INTO employee SET ?',
+            'insert into employee set ?',
             {
                 first_name: first_name,
                 last_name: last_name,
@@ -206,6 +251,43 @@ async function addEmployee() {
             (err) => {
                 if (err) throw err;
                 console.log('Successfully added employee!');
+                start();
+            }
+        );
+    });
+}
+
+async function addRole() {
+    let currentDepartments = await getDepartments();
+
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the role's title?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "What is the salary of this role?",
+            name: "salary"
+        },
+        {
+            type: "list",
+            message: "What is the department of this role?",
+            name: "department",
+            choices: getDepartmentNames(currentDepartments)
+        }, 
+    ]).then(({ title, salary, department }) => {
+        connection.query(
+            'insert into role set ?',
+            {
+                title: title,
+                salary: salary,
+                department_id: getDepartmentId(currentDepartments, department),
+            },
+            (err) => {
+                if (err) throw err;
+                console.log('Successfully added role!');
                 start();
             }
         );
